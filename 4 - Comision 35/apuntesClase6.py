d2b298d808a83385b080c1fad676904afde69934
Guido <guidopasciucco@gmail.com>
2024-02-27 20:46:04 -0300

    clases 6 varios

---
+++ 4 - Comision 35/apuntesClase6.py
@@ -0,0 +1,47 @@
+"""
+Enunciado
+Cierta empresa requiere una aplicación informática 
+para administrar los datos de su personal, del
+cual se conoce:
+- número de DNI,
+- nombre
+- apellido
+- año de ingreso.
+
+Existen dos categorías de empleados:
+- con salario fijo
+- a comisión.
+
+Los empleados a comisión tienen
+- un salario mínimo
+- un número de clientes captados
+- un monto a cobrar por cada cliente captado.
+
+El salario = clientes captados * monto por cliente
+Si el salario obtenido por los clientes
+captados no llega a cubrir el salario mínimo, cobrará
+el salario mínimo. 
+
+---
+
+Los empleados con salario fijo tienen
+- un sueldo básico
+- un porcentaje adicional
+
+porcentaje extra en función de su antigüedad: 
+- Menos de 2 años: Nada
+- De 2 a 5 años: 5% más
+- Más de 5 años: 10% más
+
+Basado en el enunciado descripto, realizá:
+A) El diagrama de clases que lo modelice, con sus relaciones, atributos y métodos.
+B) La implementación del método mostrarSalarios que imprima por consola el nombre
+completo de cada empleado junto a su salario.
+C) La implementación del método empleadoConMasClientes que devuelva al empleado con
+mayor cantidad de clientes captados (se supone único).
+
+Trabajen y ejemplifiquen con 10 instancias (creen 10 empleados con diferentes valores)
+
+"""
+
+
